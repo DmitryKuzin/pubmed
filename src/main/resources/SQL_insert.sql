fbe79fe8964fedef9b02f1e05463a0ea202cac5b
kuzin <dima.kuzin@dz.ru>
2018-03-13 07:34:27 +0300

    added heroku postgres to properties

--- src/main/resources/SQL_insert.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS customer (
-  id                   BIGINT    NOT NULL PRIMARY KEY,
-  first_name           VARCHAR(255) NOT NULL,
-  last_name          VARCHAR(255) NOT NULL
-);
